5e3887d2b65f08444d6c86ca785a5bc666efc02b
Lissa Franzini <lissa.dfranzini@gmail.com>
2026-05-27 08:51:08 -0300

    add intro message

--- week-1/projects/chatbot/chatbot.py
+++ week-1/projects/chatbot/chatbot.py
@@ -2,6 +2,9 @@ from ai_client import client, handle_client_error, ai_client
 
 chat_history = []
 
+print("Welcome to your chatbot script!")
+print("Type your question to as something, \"clear\" to clean the chat history, and \"quit\" to exit")
+
 while True:
     try:
         question = input("What's your question?").strip()
